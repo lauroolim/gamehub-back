5425b628dee3d7c8441d6e24c8ea645455cd4d67
lauroolim <lauroolimota743@gmail.com>
2024-09-14 12:13:20 -0300

    setup prisma

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { PrismaModule } from './shared/database/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    envFilePath: '.env',
+  }), PrismaModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule { }
