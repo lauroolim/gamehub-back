9a1037162122b7e908c6187c741dc195cc2a1d89
lauroolim <lauroolimota743@gmail.com>
2024-09-15 20:14:21 -0300

    to deploy -  ignore

--- prisma/generated/client/edge.js
+++ prisma/generated/client/edge.js
@@ -157,7 +157,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../..",
--- prisma/generated/client/index.js
+++ prisma/generated/client/index.js
@@ -158,7 +158,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../..",
