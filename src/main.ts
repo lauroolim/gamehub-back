2a4d4e6263888a1676fae7e62e18c5d2e94210e3
lauroolim <lauroolimota743@gmail.com>
2024-09-15 23:18:29 -0300

    refact to deploy

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,8 @@ async function bootstrap() {
       enableImplicitConversion: true,
     },
   }));
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
