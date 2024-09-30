d1d37ae498551ee3568a3a1b12af79c05a48f20f
lauroolim <lauroolimota743@gmail.com>
2024-09-30 07:48:32 -0300

    add more games
    Signed-off-by: lauroolim <lauroolimota743@gmail.com>

--- src/game/game.service.ts
+++ src/game/game.service.ts
@@ -8,6 +8,9 @@ export class GamesService implements OnModuleInit {
         { name: 'World of Warcraft', imageUrl: 'https://assets2.ignimgs.com/2014/02/14/wow-boss-compilation-wallpaperjpg-dc3b63.jpg' },
         { name: 'Age of Empires', imageUrl: 'https://cdn.ageofempires.com/aoe/wp-content/uploads/2024/02/AoEM-banner-2520x1080-2.webp' },
         { name: 'League of Legends', imageUrl: 'https://s2-ge.glbimg.com/FKUe6SrgjSlshS7Jk25FNsk2AZg=/1200x/smart/filters:cover():strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2021/6/Q/Fq6AMUTJKxZA7SVgIJBA/league-of-legends.jpg' },
+        { name: 'Grand Theft Auto', imageUrl: 'https://s2-techtudo.glbimg.com/XiktvMqc4XkevMNvk4vkAgCNvok=/0x0:695x434/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2021/A/B/7WRq3bSDevZ2py0HSE1Q/2015-01-29-gta-5.jpg' },
+        { name: 'Fortnite', imageUrl: 'https://wallpapers.com/images/featured/fortnite-pictures-b5kfcchwazwiz3cs.jpg' },
+        { name: 'Counter Strike 2', imageUrl: 'https://media.steampowered.com/apps/csgo/blog/images/fb_image.png?v=6' },
     ];
 
     constructor(private readonly prisma: PrismaService) { }
