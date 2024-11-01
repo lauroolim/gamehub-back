e1a80840de7f8609cb414cf652d25ce5c564f275
lauroolim <lauroolimota743@gmail.com>
2024-10-31 23:28:11 -0300

    fix: import modules S3

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,12 +4,15 @@ import { UserController } from './user.controller';
 import { PrismaModule } from '../shared/database/prisma.module';
 import { PrismaService } from '../shared/database/prisma.service';
 import { FriendshipModule } from '../friendship/friendship.module';
+import { S3Module } from '../shared/services/s3.module';
+import { S3Service } from '../shared/services/s3.service';
+
 
 
 @Module({
-  imports: [PrismaModule, FriendshipModule],
+  imports: [PrismaModule, FriendshipModule, S3Module],
   controllers: [UserController],
-  providers: [UserService, PrismaService],
+  providers: [UserService, PrismaService, S3Service],
   exports: [UserService],
 })
 export class UserModule { }
